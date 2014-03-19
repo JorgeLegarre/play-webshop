58d121cadfdc4bd0f17669021a088c06fb58caa5
JorgeLegarre <enkidugan@gmail.com>
2014-03-19 09:43:06 +0100

    JLP --> Ebean

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,27 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table category (
+  id                        integer not null,
+  name                      varchar(255),
+  staff_responsible         integer,
+  constraint pk_category primary key (id))
+;
+
+create sequence category_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists category;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists category_seq;
+
